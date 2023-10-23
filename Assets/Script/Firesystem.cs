f14b66560da59420f7b3826f17b283393f48158c
xingling1011 <91235677+xingling1011@users.noreply.github.com>
2023-10-23 11:02:51 +0800

    Update 1023

--- Assets/Script/Firesystem.cs
+++ Assets/Script/Firesystem.cs
@@ -1,8 +1,54 @@
+using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.InputSystem.Processors;
+using UnityEngine.Windows;
 
 public class Firesystem : MonoBehaviour
 {
-   public int magazineCount;
-   public int bulletTotal;
-   public int bulletCurrent;
+    [Header("彈夾子彈數量"), Range(0, 10)]
+    public int magazineCount = 7;
+    [Header("子彈總數"), Range(20, 200)]
+    public int bulletTotal = 28;
+    [Header("目前子彈數量"), Range(0, 10)]
+    public int bulletCurrent = 7;
+    [Header("子彈預製物")]
+    public GameObject prefabBullet;
+    [Header("子彈生成點")]
+    public Transform pointSpawnBullet;
+
+    public bool openDoor = true;
+    public bool isDead = false;
+
+    private void Awake()
+    {
+        //print("<color=green>喚醒事件</color>");
+        
+        
+     }
+
+    private void Start()
+    {
+        //print("<color=red>開始事件</color>");
+
+        if (openDoor) 
+        {
+            print("已經開門");
+        }
+
+        if (isDead) 
+        {
+            print("已經死亡");
+        }
+    }
+    private void Update()
+    {
+        //print("<color=yellow>更新事件</color>");
+
+       if (UnityEngine.Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            Instantiate(prefabBullet);
+        }
+    }
+
+    
 }
